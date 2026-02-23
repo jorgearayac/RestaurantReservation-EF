5694401e1881171e6a54b149c1ba02c926d27dc9
jorgearayac <araya.cepeda.jorge@gmail.com>
2026-02-23 16:42:30 -0300

    Create/Update/Delete methods implemented

--- RestaurantReservation/Program.cs
+++ RestaurantReservation/Program.cs
@@ -1,7 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-using RestaurantReservation.Db.Context;
+﻿using RestaurantReservation.Db.Context;
+using RestaurantReservation.Db.Models;
+using RestaurantReservation.Db.Services;
+using System.ComponentModel.DataAnnotations;
 
-using (var context = new RestaurantReservationDbContext())
+class Program
 {
-    context.Database.EnsureCreated();
+    static async Task Main(string[] args)
+    {
+        using var context = new RestaurantReservationDbContext();
+        var customerService = new CustomerService(context);
+
+        var newCustomer = new Customer { FirstName = "Async", LastName = "Await", Email = "async@test.cl", PhoneNumber = "555555555" };
+        await customerService.Create(newCustomer);
+        Console.WriteLine($"Customer created: {newCustomer.FirstName} {newCustomer.LastName} (ID: {newCustomer.CustomerId})");
+    }
 }
